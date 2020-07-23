37864dbcad1633becb2496adb8d0ec72ff23c5bb
WLL-1017065322 <1017065322@qq.com>
2020-07-23 13:58:28 +0800

    start

---
+++ vue.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  devServer: {
+    progress: false
+  }
+}
+
+// vue.config.js
+// ...
+
+// 添加包
+// module.exports = {
+//   chainWebpack: config => {
+//     // remove vue-cli-service's progress output
+//     config.plugins.delete('progress')
+//     // optionally replace with another progress output plugin
+//     // `npm i -D simple-progress-webpack-plugin` to use
+//     config
+//       .plugin('simple-progress-webpack-plugin')
+//       .use(require.resolve('simple-progress-webpack-plugin'), [
+//         {
+//           format: 'minimal' // options are minimal, compact, expanded, verbose
+//         }
+//       ])
+//   }
+// }
